1d456f54b7a1133faf033adbe1147146b1888034
jonas <jonasvaerewijck@gmail.com>
2025-11-30 14:44:53 +0100

    Add temp levelmanager, crosshair and make healthbar stay in top left

---
+++ TinyArena/Assets/Scripts/LevelControllerTemp.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LevelControllerTemp : MonoBehaviour
+{
+    private float spawnDelay = 10;
+    private float waitingTime = 0;
+
+    [SerializeField] GameObject enemy;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitingTime + Time.deltaTime > spawnDelay)
+        {
+            Instantiate(enemy, new Vector3(-10, 1, 20), Quaternion.identity);
+            waitingTime = 0;
+            if (spawnDelay > 2f)
+                spawnDelay -= 0.5f;
+        } 
+        else 
+            waitingTime += Time.deltaTime;
+    }
+}
