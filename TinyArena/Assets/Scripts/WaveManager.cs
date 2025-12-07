69eccd066847ec4950e932b1829bdaee6c26996b
jonas <jonasvaerewijck@gmail.com>
2025-12-07 17:48:33 +0100

    fix gamemanager bugs

--- TinyArena/Assets/Scripts/WaveManager.cs
+++ TinyArena/Assets/Scripts/WaveManager.cs
@@ -1,3 +1,7 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,22 +11,77 @@ public class WaveManager : MonoBehaviour
     
     [SerializeField] Transform[] spawnPoints;
     [SerializeField] GameObject enemy;
-    private GameObject Ring1;
-    private GameObject Ring2;
-    private GameObject Ring3;
-    private GameObject Ring4;
+    private List<GameObject> Rings = new List<GameObject>();
+    [SerializeField] Material normal;
+    [SerializeField] Material translucent;
+    private Vector3 spawnPoint;
+
+    private float delayRemove = 2f;
+    private float waitingTimeRemove = 0f;
+    private float delayEnemy = 10f;
+    private float waitingTimeEnemy = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Ring1 = GameObject.FindWithTag("Ring1");
-        Ring2 = GameObject.FindWithTag("Ring2");
-        Ring3 = GameObject.FindWithTag("Ring3");
-        Ring4 = GameObject.FindWithTag("Ring4");
+        // should be removed after gamemanager exists
+        Cursor.lockState = CursorLockMode.Locked;
+        Rings.Add(GameObject.FindWithTag("Ring1"));
+        Rings.Add(GameObject.FindWithTag("Ring2"));
+        Rings.Add(GameObject.FindWithTag("Ring3"));
+        Rings.Add(GameObject.FindWithTag("Ring4"));
+        spawnPoint = GameObject.FindWithTag("SpawnPoint").GetComponent<Transform>().position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (waitingTimeRemove + Time.deltaTime > delayRemove)
+        {
+            StartCoroutine(DropRandomTile());
+            waitingTimeRemove = 0;
+            if (delayRemove > 1f)
+                delayRemove -= 0.2f;
+        }
+        else
+            waitingTimeRemove += Time.deltaTime;
+
+        if (waitingTimeEnemy + Time.deltaTime > delayEnemy)
+        {
+            Instantiate(enemy, spawnPoint, Quaternion.identity);
+            waitingTimeEnemy = 0;
+            if (delayEnemy > 3f)
+                delayEnemy -= 0.5f;
+        }
+        else
+            waitingTimeEnemy += Time.deltaTime;
+    }
+
+    // rotates a specific ring 
+    IEnumerator RotateRing(int ringindex, float RotationAmmount)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator DropRandomTile()
+    {
+        int ringIndex = Mathf.FloorToInt(Random.Range(0, 4));
+        int tileIndex = Mathf.FloorToInt(Random.Range(0, 16));
+        GameObject ring = Rings[ringIndex];
+        List<Transform> tiles = ring.GetComponentsInChildren<Transform>().Where(x => !x.gameObject.Equals(ring)).ToList();
+        GameObject tile = tiles[tileIndex].gameObject;
+        Debug.Log($"name: {tile.name}");
+        if (tile.Equals(ring) || tile.GetComponent<MeshRenderer>().material.Equals(translucent))
+        {
+            yield break;
+        }
         
+        tile.GetComponent<MeshRenderer>().material = translucent;
+        yield return new WaitForSeconds(2f);
+        tile.GetComponent<MeshCollider>().enabled = false;
+        tile.GetComponent<MeshRenderer>().enabled = false;
+        yield return new WaitForSeconds(10f);
+        tile.GetComponent<MeshRenderer>().material = normal;
+        tile.GetComponent<MeshCollider>().enabled = true;
+        tile.GetComponent<MeshRenderer>().enabled = true;
     }
 }
