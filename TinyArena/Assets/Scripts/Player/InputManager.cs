4c720c169d0e1e0daa2643beb3e4d7c7e98affb1
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2025-12-15 13:02:01 +0100

    added assets

--- TinyArena/Assets/Scripts/Player/InputManager.cs
+++ TinyArena/Assets/Scripts/Player/InputManager.cs
@@ -1,46 +1,114 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
-public class InputManager : MonoBehaviour
+public class GameTestManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private PlayerInput.OnFootActions onFoot;
+    private GameObject gameOverUI;
+    private Canvas gameOverUICanvas;
+    private Scene currentScene;
+    private GameObject player;
+    private PlayerHealth playerHealth;
 
-    private PlayerMotor motor;
-    private PlayerLook look;
-    private PlayerShoot shoot;
-    private void Awake()
+    private GameState currentState;
+    public GameState CurrentState
     {
-        playerInput = new PlayerInput();
-        onFoot = playerInput.OnFoot;
+        get => currentState;
+        private set
+        {
+            if (currentState != value)
+            {
+                currentState = value;
+                OnGameStateChanged?.Invoke(currentState);
+            }
+        }
+    }
+    public static GameManager Instance;
+    public delegate void GameStateChanged(GameState newState);
+    public event GameStateChanged OnGameStateChanged;
+    public string gameplaySceneName { get; set; }
 
-        motor = GetComponent<PlayerMotor>();
-        look = GetComponent<PlayerLook>();
-        shoot = GetComponent<PlayerShoot>();
-
-        onFoot.Jump.performed += ctx => motor.Jump();
-        onFoot.Shoot.performed += ctx => shoot.Shoot();
-        onFoot.Reload.performed += ctx => shoot.Reload();
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
     }
 
-    private void FixedUpdate()
+    void Start()
     {
-        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        ChangeState(GameState.Playing);
+
     }
 
-    private void LateUpdate()
+    void Update()
     {
-        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+        switch (currentState)
+        {
+            case GameState.MainMenu:
+                // Logic for entering the Main Menu
+                break;
+            case GameState.Playing:
+                // Logic for starting Gameplay
+                HandlePlayingGameState();
+                break;
+            case GameState.Paused:
+                // Logic for Pausing the game
+                HandlePausedGameState();
+                break;
+            case GameState.GameOver:
+                // Logic for the Game Over sequence
+                HandleGameOverGameState();
+                break;
+        }
     }
 
-    private void OnEnable()
+    private void ChangeState(GameState newState)
     {
-        onFoot.Enable();
+        if (CurrentState == newState) return;
+        CurrentState = newState;
     }
 
-    private void OnDisable()
+    private void HandlePlayingGameState()
     {
-        onFoot.Disable();
+        if (SceneManager.GetSceneByName("GameLevelScene").isLoaded)
+        {
+            currentScene = SceneManager.GetSceneByName("GameLevelScene");
+            Cursor.lockState = CursorLockMode.Locked;
+            player = GameObject.FindGameObjectWithTag("Player");
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                if (player.health <= 0 )
+            }
+        }
+        SceneManager.LoadScene("GameLevelScene", LoadSceneMode.Additive);
     }
 
+    private void HandlePausedGameState()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    private void HandleGameOverGameState()
+    {
+        if (gameOverUICanvas is null)
+            Debug.LogError("GameOverCanvas could not be found.");
+        else
+        {
+            gameOverUICanvas.enabled = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0.0f;
+        }
+    }
+}
+
+public enum GameState
+{
+    MainMenu,
+    Playing,
+    Paused,
+    GameOver
 }
