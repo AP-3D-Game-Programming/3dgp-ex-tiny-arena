632bff4892097fe3ed2bb7349bf411077a3397d0
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2025-12-31 13:46:42 +0100

    fixed Input manager

--- TinyArena/Assets/Scripts/Player/InputManager.cs
+++ TinyArena/Assets/Scripts/Player/InputManager.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameTestManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    private GameObject gameOverUI;
-    private Canvas gameOverUICanvas;
-    private Scene currentScene;
-    private GameObject player;
-    private PlayerHealth playerHealth;
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
 
     private PlayerMotor motor;
     private PlayerLook look;
@@ -16,34 +13,8 @@ public class GameTestManager : MonoBehaviour
 
     private void Awake()
     {
-        get => currentState;
-        private set
-        {
-            if (currentState != value)
-            {
-                currentState = value;
-                OnGameStateChanged?.Invoke(currentState);
-            }
-        }
-    }
-    public static GameManager Instance;
-    public delegate void GameStateChanged(GameState newState);
-    public event GameStateChanged OnGameStateChanged;
-    public string gameplaySceneName { get; set; }
-
-    void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else Destroy(gameObject);
-    }
-
-    void Start()
-    {
-        ChangeState(GameState.Playing);
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
 
         motor = GetComponent<PlayerMotor>();
         look = GetComponent<PlayerLook>();
@@ -61,44 +32,5 @@ public class GameTestManager : MonoBehaviour
 
         // Switch spells
         onFoot.ChangeWeapon.performed += ctx => spellManager.NextSpell();
-    }
-
-    private void ChangeState(GameState newState)
-    {
-        if (CurrentState == newState) return;
-        CurrentState = newState;
-    }
-
-    private void HandlePlayingGameState()
-    {
-        if (SceneManager.GetSceneByName("GameLevelScene").isLoaded)
-        {
-            currentScene = SceneManager.GetSceneByName("GameLevelScene");
-            Cursor.lockState = CursorLockMode.Locked;
-            player = GameObject.FindGameObjectWithTag("Player");
-            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                if (player.health <= 0 )
-            }
-        }
-        SceneManager.LoadScene("GameLevelScene", LoadSceneMode.Additive);
-    }
-
-    private void HandlePausedGameState()
-    {
-        Time.timeScale = 0.0f;
-    }
-
-    private void HandleGameOverGameState()
-    {
-        if (gameOverUICanvas is null)
-            Debug.LogError("GameOverCanvas could not be found.");
-        else
-        {
-            gameOverUICanvas.enabled = true;
-            Cursor.lockState = CursorLockMode.None;
-            Time.timeScale = 0.0f;
-        }
     }
 }
