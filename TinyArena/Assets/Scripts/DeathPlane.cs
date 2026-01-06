7414cd6cbfdd7617679db376e30077d14df9d99a
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2026-01-06 17:31:36 +0100

    Hotfix

--- TinyArena/Assets/Scripts/DeathPlane.cs
+++ TinyArena/Assets/Scripts/DeathPlane.cs
@@ -4,8 +4,6 @@ using UnityEngine.UI;
 
 public class DeathPlane : MonoBehaviour
 {
-<<<<<<<< HEAD:TinyArena/Assets/Scripts/DeathPlane.cs
-    [SerializeField] Image gameOverText;
     GameObject player;
     PlayerHealth playerHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,28 +13,11 @@ public class DeathPlane : MonoBehaviour
         playerHealth = player.GetComponent<PlayerHealth>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-========
-    private PlayerHealth health;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        health = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
->>>>>>>> waves:TinyArena/Assets/Scripts/KillZone.cs
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
-<<<<<<<< HEAD:TinyArena/Assets/Scripts/DeathPlane.cs
             playerHealth.TakeDamage(10000f);
-========
-            health.TakeDamage(100000f);
->>>>>>>> waves:TinyArena/Assets/Scripts/KillZone.cs
         }
         else if (other.gameObject.CompareTag("Enemy"))
         {
